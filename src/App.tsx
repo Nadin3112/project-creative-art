6e8176500dd31ae4da2478248f76503b4cfd95ad
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-01-06 16:40:26 +0300

    подключает header and hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
+import { Header } from './layout/header/Header';
+import { Hero } from './layout/sections/hero/Hero';
+
 
 
 function App() {
     return (
         <div className="App">
+            <Header />
+            <Hero/>
         </div>
     );
 }
 
 export default App;
-
